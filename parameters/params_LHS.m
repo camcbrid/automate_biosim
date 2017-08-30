c9ad469ca0aecd2ec72b45592c51722d054e316e
camcbrid <camcbrid@umich.edu>
2017-08-29 22:20:30 -0400

    add latin hypercube sampling functionality

--- parameters/params_LHS.m
+++ parameters/params_LHS.m
@@ -1,20 +1,16 @@
-function p = params_LHS(A)
+function [p,prng] = params_LHS(A,numsamples)
 %assign parameters for production to each edge and degradation parameters
 %for each protein. Takes in the weighted adacjency matrix, outputs
 %parameter struct for functions h, g, a, and L to be used by makefuns.m
+%using a specified distribution (normal, lognormal, uniform, etc)
 
-if nargin < 1
-    error('not enough inputs')
+%defaults
+if nargin < 2
+    numsamples = 100;
+    if nargin < 1
+        error('not enough inputs')
+    end
 end
-B = (A ~= 0);                       %logical adjacency matrix
-n = min(size(A));                   %number of nodes
-m = sum(B(A ~= 0));                 %number of edges
-
-%extract weighting of each edge
-edge_weights = A(A ~= 0);
-
-%output parameter struct
-p = struct;
 
 %set the ranges for each parameter
 k1rng = [195,275];      % [/hr] rate of transcription (195 to 275)
@@ -35,87 +31,139 @@ k3rng = [270,270];      % [/hr] catalytic rate of degradation by protease
 Ptotrng = [0,0];        % [nM] total amount of protease
 K3rng = log10([1200,1200]);    % [nM] protein with protease binding constant
 
-%pick random parameters within each range
-p.k1 = rng2out(k1rng);          %TX rate
-p.k2 = rng2out(k2rng);          %TL rate
-p.k3 = rng2out(k3rng);          %degradation rate by protease
-p.delta1 = rng2out(delta1rng);  %mRNA dilution/degradation
-p.delta2 = rng2out(delta2rng);  %protein dilution
-p.RNAP = rng2out(RNAPrng);      %total RNAP
-p.Ribo = rng2out(Riborng);      %total ribosomes
-p.Ptot = rng2out(Ptotrng);      %total protease
-p.DNA = rng2out(DNArng);        %total DNA
-p.K0 = 10.^rng2out(K0rng);      %TF/DNA binding cnst
-p.K1 = 10.^rng2out(K1rng);      %RNAP/(TF/DNA) binding cnst
-p.Kp = 10.^rng2out(Kprng);      %RNAP/DNA leaky binding cnst
-p.K2 = 10.^rng2out(K2rng);      %mRNA/ribosome binding cnst
-p.s = rng2out(nrng);            %coopertivity
-p.K3 = rng2out(K3rng);          %protein/protease binding cnst
 
-%production parameters
-hparams = struct;
-hparams.J2 = num2cell(0*ones(1,n));
-%resource demand
-hparams.J = num2cell(p.DNA./p.Kp.*(1+p.k1*p.RNAP./(p.K2*p.delta1)));
-%leakiness
-hparams.T = num2cell(p.k2*p.k1*p.RNAP*p.Ribo*p.DNA./(p.Kp.*p.K2.*p.delta1));
-%coopertivitity
-hparams.n = num2cell(p.s);
 
-%loop through each edge
-for ii = 1:m
-    if edge_weights(ii) > 0
-        %activation; max: T*a/b; min: T
-        %activation strength
-        hparams.a{ii} = (max([p.Kp/p.K1,p.K1/p.Kp])/p.K0)*abs(edge_weights(ii));
-        hparams.b{ii} = 1./p.K0;                       %1/binding constant
-        %disp('activation')
-    else
-        %repression; max: T; min: T*a/b
-        %activation strength
-        hparams.a{ii} = (min([p.Kp/p.K1,p.K1/p.Kp])/p.K0)*abs(edge_weights(ii));
-        hparams.b{ii} = 1./p.K0;                       %1/binding constant
-        %disp('repression')
+%setup
+B = (A ~= 0);               %logical adjacency matrix
+nodevec = [0,cumsum(sum(B,1))]; %indicies of nodes in the edge counter
+indvec = sum(B,1);          %number of incoming connections to node i
+n = min(size(A));           %number of nodes
+m = sum(B(A ~= 0));         %number of edges
+edge_weights = A(A ~= 0);   %extract weighting of each edge
+
+%need to pick for all duples of input combinations
+if m > 1
+    %find number of pairs of edges incident to node jj
+    twocombos = zeros(n,1);
+    for jj = 1:n
+        if indvec(jj) > 1
+            twocombos(jj) = nchoosek(indvec(jj),2);
+        end
     end
+    %number of additonal parameters required:
+    q = sum(twocombos);
+else
+    q = 0;
 end
 
-%shared degradation parameters degradation applies to all nodes (set K to
-%inf if not degradated) could define index sets to use if had multiple
-%proteases present
-gparams = struct;
-gparams.k = num2cell(p.k3);         %catalytic rate of degradation
-gparams.Ptot = num2cell(p.Ptot);    %total amount of protease
-gparams.K = num2cell(p.K3);         %binding constant for each node
+%create latin hypercube
+X = lhsdesign(numsamples, 8 + 4*n + 3*m + q);
+p = cell(0);
 
-%combining into output parameter struct
-p.delta = p.delta2;             %dilution rate constant
-p.n = n;                        %number of nodes
-p.m = m;                        %number of edges
-p.h = hparams;                  %production parameter struct
-p.a = hparams;                  %production resource sharing struct
-p.g = gparams;                  %degradation resource sharing struct
+%loop through LHC samples
+for jj = 1:numsamples
+    
+    k = 1;                                      %init node counter
+    %pick parameters within each range
+    p{jj} = struct;                             %output parameter struct
+    p{jj}.k1 = rng2lhs(k1rng,X(jj,1));          %TX rate
+    p{jj}.k2 = rng2lhs(k2rng,X(jj,2));          %TL rate
+    p{jj}.k3 = rng2lhs(k3rng,X(jj,3));          %degradation rate by protease
+    p{jj}.delta1 = rng2lhs(delta1rng,X(jj,4));  %mRNA dilution/degradation
+    p{jj}.delta2 = rng2lhs(delta2rng,X(jj,5));  %protein dilution
+    p{jj}.RNAP = rng2lhs(RNAPrng,X(jj,6));      %total RNAP
+    p{jj}.Ribo = rng2lhs(Riborng,X(jj,7));      %total ribosomes
+    p{jj}.Ptot = rng2lhs(Ptotrng,X(jj,8));      %total protease
+    p{jj}.DNA = rng2lhs(DNArng,X(jj,9:8+n));    %total DNA
+    p{jj}.Kp = rng2lhs(Kprng,X(jj,9+n:8+2*n),'log');    %RNAP/DNA leaky binding cnst
+    p{jj}.K2 = rng2lhs(K2rng,X(jj,9+2*n:8+3*n),'log');    %mRNA/ribosome binding cnst
+    p{jj}.K3 = rng2lhs(K3rng,X(jj,9+3*n:8+4*n),'log');    %protein/protease binding cnst
+    p{jj}.K0 = rng2lhs(K0rng,X(jj,9+4*n:8+4*n+m),'log');   %TF/DNA binding cnst
+    p{jj}.K1 = rng2lhs(K1rng,X(jj,9+4*n+m:8+4*n+2*m),'log');   %RNAP/(TF/DNA) binding cnst
+    p{jj}.s = rng2lhs(nrng,X(jj,9+4*n+2*m:8+4*n+3*m));          %coopertivity
+    p{jj}.K02 = rng2lhs(K0rng,X(jj,9+4*n+2*m:8+4*n+3*m+q),'log');%TF/DNA binding cnst for two TFs at once
+    
+    %production parameters
+    hparams = struct;
+    %resource demand
+    hparams.J = p{jj}.DNA./p{jj}.Kp.*(1+p{jj}.k1*p{jj}.RNAP./(p{jj}.K2*p{jj}.delta1));
+    %leakiness
+    hparams.T = p{jj}.k2*p{jj}.k1*p{jj}.RNAP*p{jj}.Ribo*p{jj}.DNA./(p{jj}.Kp.*p{jj}.K2.*p{jj}.delta1);
+    %coopertivitity
+    hparams.n = p{jj}.s;
+    %loop through each edge
+    for ii = 1:m
+        if edge_weights(ii) > 0
+            %activation; max: T*a/b; min: T
+            hparams.a(ii) = max([p{jj}.Kp(k)/p{jj}.K1(ii),p{jj}.K1(ii)/p{jj}.Kp(k)])*...
+                abs(edge_weights(ii))/p{jj}.K0(ii); %activation strength
+            hparams.b(ii) = 1./p{jj}.K0(ii);        %1/binding constant
+        else
+            %repression; max: T; min: T*a/b
+            hparams.a(ii) = min([p{jj}.Kp(k)/p{jj}.K1(ii),p{jj}.K1(ii)/p{jj}.Kp(k)])/...
+                (abs(edge_weights(ii))*p{jj}.K0(ii)); %activation strength
+            hparams.b(ii) = 1./p{jj}.K0(ii);        %1/binding constant
+        end
+        %advance the node counter
+        if any(ii == nodevec)
+            %if the promoter has non-exclusive TF binding
+            if indvec(k) > 1
+                %allocate space for each node's c's and d's
+                z = [0;cumsum(twocombos)];  %cumulative number of pairs of edges for each node
+                rinds = nchoosek(nodevec(k)+1:nodevec(k+1),2);  %indicies of pairs of each edge
+                for r = 1:(z(k+1) - z(k))
+                    s = z(k) + r;   %output counter
+                    %assign parameters for multiple protein binding to promoter
+                    d = 1./(p{jj}.K02(s)*p{jj}.K0(rinds(r,1))) + 1./(p{jj}.K02(s)*p{jj}.K0(rinds(r,2)));
+                    hparams.c(s) = max([p{jj}.Kp(k)/p{jj}.K1(ii),p{jj}.K1(ii)/p{jj}.Kp(k)])*d;
+                    hparams.d(s) = d;
+                end
+            end
+            %advance the node counter
+            k = k + 1;
+        end
+    end
+    
+    %shared degradation parameters degradation applies to all nodes (set K to
+    %inf if not degradated) could define index sets to use if had multiple
+    %proteases present
+    gparams = struct;
+    gparams.k = p{jj}.k3;       %catalytic rate of degradation
+    gparams.Ptot = p{jj}.Ptot;  %total amount of protease
+    gparams.K = p{jj}.K3;       %binding constant for each node
+    
+    %combining into output parameter struct
+    p{jj}.delta = p{jj}.delta2;     %dilution rate constant
+    p{jj}.n = n;                %number of nodes
+    p{jj}.m = m;                %number of edges
+    p{jj}.h = hparams;          %production parameter struct
+    p{jj}.a = hparams;          %production resource sharing struct
+    p{jj}.g = gparams;          %degradation resource sharing struct
+    
+end
+
+%record ranges
+prng = struct;
+prng.k1rng = k1rng;             prng.k2rng = k2rng;
+prng.delta1rng = delta1rng;     prng.delta2rng = delta2rng;
+prng.DNArng = DNArng;           prng.RNAPrng = RNAPrng;
+prng.Riborng = Riborng;         prng.nrng = nrng;
+prng.K0rng = K0rng;             prng.K1rng = K1rng;
+prng.Kprng = Kprng;             prng.K2rng = K2rng;
+prng.k3rng = k3rng;             prng.Ptotrng = Ptotrng;
+prng.K3rng = K3rng;
 
 
-function out = rng2out(p_rng,n,distname)
-%generates a random value within range. p_rng is a vector containing the
-%upper and lower bounds for the range, n is the length of output, uniform
-%is a binary option that, if true, generates an array of length n with
-%identical elements
+function out = rng2lhs(p_rng,lhsweight,scale)
+%use weighting from latin hypercube to select parameters
 
-%defaults
 if nargin < 3
-    distname = 'logn';
-    if nargin < 2
-        n = 1;
-    end
+    scale = '';
 end
 
-if strcmp(distname,'norm')
-    mu = mean(p_rng);
-    sigma = (max(p_rng) - min(p_rng))/2;
-    out = normrnd(mu,sigma,[n,1]);
-elseif ~strcmp(distname,'norm')
-    mu = mean(p_rng);
-    sigma = (max(p_rng) - min(p_rng))/2;
-    out = random(distname,mu,sigma,[n,1]);
+%create sample
+if strcmp(scale,'log')
+    out = 10.^(p_rng(1) + (p_rng(2) - p_rng(1)).*lhsweight(:)');
+else
+    out = p_rng(1) + (p_rng(2) - p_rng(1)).*lhsweight(:)';
 end
